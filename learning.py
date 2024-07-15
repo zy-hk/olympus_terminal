ef35ea767bc9373a7d101590df7150e9bde8a0c7
zyhk <zoehkyim.kikiyim@gmail.com>
2024-07-15 22:42:27 +0100

    Update learning.py

--- learning.py
+++ learning.py
@@ -14,7 +14,7 @@ class User:
         print("Hello " + self.username)
 '''
 passcode = "thisisamystery"
-password = "breakfast"
+password = "lunch:)hour"
 
 
 def account_create():
@@ -28,7 +28,7 @@ def account_create():
     g.close()
     print("You are now registered as a user, " + un)
     print("You currently have access authority: level 1")
-    password = "breakfast"
+    password = "lunch:)hour"
     print("The password is: " + password)
     print("Do not forget it.")
     return un
@@ -61,6 +61,7 @@ def todo(action):
     if action == "leave":
         g.write("\n")
         g.write(username + " left")
+        g.write("\n")
         exit()
     if action == "sea":
         folder_path = '/allfiles'
@@ -117,13 +118,89 @@ def todo(action):
         g.write("\n")
         g.write(username + " checked padlock")
         return True
+    if action == "board":
+        bo = open("board.txt", "r")
+        print(bo.read())
+        bo.close()
+        g.write("\n")
+        g.write(username + "checked the public board")
+        return True
     if action == "records":
         h = open("records.txt", "r")
         print(h.read())
+        h.close()
         g.write("\n")
         g.write("admin checked records")
         return True
-    #UP TO HERE
+    if action == "users":
+        u = open("users.txt", "r")
+        print(u.read())
+        u.close()
+        g.write("\n")
+        g.write("admin checked users.txt")
+        return True
+    if action == "views":
+        v = open("admin.txt", "r")
+        print(v.read())
+        v.close()
+        g.write("\n")
+        g.write("admin checked admin.txt")
+        return True
+    if action == "level2":
+        lt = open("level2.txt", "r")
+        print(lt.read())
+        lt.close()
+        g.write("\n")
+        g.write("admin checked level2.txt")
+        return True
+    if action == "inbox":
+        ib = open("admin_email.txt", "r")
+        print(ib.read())
+        ib.close()
+        g.write("\n")
+        g.write("admin checked admin email")
+        return True
+    if action == "second":
+        at = open("level2.txt", "a")
+        print("please type username to increase access")
+        at.write("\n")
+        newname = input("OT&/admin> ")
+        at.write(newname)
+        at.close()
+        g.write("\n")
+        g.write("admin increased " + newname + " to level 2 access")
+        return True
+    if action == "post":
+        bo = open("board.txt", "a")
+        bo.write("--------------------------------------------------------------------------------------------")
+        bo.write("\n")
+        bo.write("Post by " + username + " : " + input("OT&/" + username + " !!POST TITLE?!> "))
+        bo.write("\n")
+        print("OT&/" + username + " !!WRITE POST CONTENT BELOW!!> ")
+        bo.write("\n")
+        bo.write(input("OT&/" + username + " > "))
+        bo.write("\n")
+        bo.write("--------------------------------------------------------------------------------------------")
+        bo.write("\n")
+        bo.close()
+        print("OT&/" + username + " !!POSTED!!> ")
+        g.write("\n")
+        g.write(username + " added a post to public board.")
+    if action == "email":
+        ae = open("admin_email.txt", "a")
+        ae.write("\n")
+        ae.write("Message from  " + username)
+        ae.write("\n")
+        print("OT&/" + username + " !!WRITE MESSAGE BELOW!!> ")
+        ae.write("\n")
+        ae.write(input("OT&/" + username + " > "))
+        ae.write("\n")
+        ae.write("-----------------------")
+        ae.write("\n")
+        ae.close()
+        print("OT&/" + username + " !!SENT!!> ")
+        g.write("\n")
+        g.write(username + " sent a message to admin.")
     g.close()
 
     
@@ -145,6 +222,8 @@ else: tmpvar = input("Sign in or Create Account? (s/c)")
 
 
 if correct == True:
+    with open("level2.txt", "r") as w:
+        second = w.read().splitlines()
     username = input("Please re-enter username: ")
     if username == "admin":
         print("Hello, admin")
@@ -160,7 +239,26 @@ if correct == True:
 
         for i in range(0,30):
             todo(input("OT&/admin> ")) 
-        
+
+    elif username in second:
+        print("Hello, " + username)
+        g = open("records.txt", "a")
+        g.write("\n")
+        g.write(username + " logged in")
+        g.close()
+        print("You currently have level 2 access")
+        print("Limit on command excecution extended to 20")
+        f = open("commands.txt", "r")
+        print(f.read())
+        f.close()
+        s = open("level2_co.txt", "r")
+        print(s.read())
+        s.close()
+
+        for i in range(0,20):
+            todo(input("OT&/" + username + "> ")) 
+
+
     else:
         print("Hello, " + username)
         g = open("records.txt", "a")
